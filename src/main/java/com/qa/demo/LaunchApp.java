e71b673b29d0459d0c8d9dfeb719d0f2a3bb056f
sujatakaname <59989766+sujatakaname@users.noreply.github.com>
2020-01-17 11:41:33 +0530

    Update LaunchApp.java

--- src/main/java/com/qa/demo/LaunchApp.java
+++ src/main/java/com/qa/demo/LaunchApp.java
@@ -5,5 +5,6 @@ public class LaunchApp {
 	public static void main(String[] args) {
 	
          System.out.println("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+		System.out.println("Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
 	}
 }
